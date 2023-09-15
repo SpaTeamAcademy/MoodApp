ae9c9364bdad8fa6f17700f7ad336fc00e475d05
Katrin Hinterberger <k.hinterberger@outlook.com>
2023-09-15 10:22:20 +0200

    fetch added to JS file to access keywords

--- script.js
+++ script.js
@@ -2,3 +2,23 @@ function display() {
     //console.log to test function//
     console.log("hello i am a color");
 }
+
+//fetches moods.json so we can access the data
+fetch("./moods.json")
+    .then(function (response) {
+    return response.json();
+    })
+    .then(function (data){ //do something with the data
+        const moodList = document.getElementById("moodList");
+
+        for (let i = 0; i < data.Moods.length; i++) {
+            let moodItem = data.Moods[i].keywords[0];
+            let listItem = document.createElement("li");
+            listItem.textContent = moodItem;
+            moodList.appendChild(listItem);
+          };
+    })
+    .catch(function (error){
+    console.error("Something went wrong with retrieving the moods.");
+    console.error(error);
+    })
