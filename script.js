e485feb50bffe92598b9f62394c9e9d7e1621116
Io Bosse <io.bosse@protonmail.com>
2023-09-19 11:43:29 +0200

    implemented the fetched variable into the existing functions

--- script.js
+++ script.js
@@ -1,33 +1,31 @@
 let moodsData;
 
+//fetches moods.json so we can access the data
 fetch("./moods.json")
 .then(res => res.json())
 .then(data =>{
     moodsData = data;
 }).then(() => {
     console.log(moodsData);
-});
-
-//fetches moods.json so we can access the data
-fetch("./moods.json")
-    .then(function (response) {
-    return response.json();
-    })
-    .then(function (data){ //now: do something with the data
-
-        let moodElements = document.querySelectorAll(".keywords"); // selects all elements with the class="keywords"
-
-        for (let i in data.Moods) { //iterates thru data.Moods and appends the first keyword of each mood to "p"
-                let moodItem = data.Moods[i].keywords[0];
-                let listItem = document.createElement("p");
-                listItem.textContent = moodItem;
-                moodElements[i].appendChild(listItem);
-            }
-    })
-    .catch(function (error){
+    showKeywords();
+})
+.catch(function (error){
     console.error("Something went wrong with retrieving the moods.");
     console.error(error);
-    })
+});
+
+
+function showKeywords(){
+    let moodElements = document.querySelectorAll(".keywords"); // selects all elements with the class="keywords"
+
+    for (let i in moodsData.Moods) { //iterates thru data.Moods and appends the first keyword of each mood to "p"
+        let moodItem = moodsData.Moods[i].keywords[0];
+        let listItem = document.createElement("p");
+        listItem.textContent = moodItem;
+        moodElements[i].appendChild(listItem);
+    }
+}
+
 
 /*opens and closes pop up*/
 const modal = document.querySelector(".keywordPopup");
@@ -57,39 +55,35 @@ function display(color){ //is called using onclick for now, gets the button's ID
     body.innerHTML = "";
     mind.innerHTML = "";
     strategy.innerHTML = "";
-    fetch('moods.json')
-    .then(res => res.json())
-    .then(function(data){
-      document.getElementById("colorName").innerText = color;
-      
-        for(let i in data.Moods){ //each for loop within this one fills one of the lists
-            if(data.Moods[i].color === color){
-                for(let j in data.Moods[i].keywords){
-                  let el = document.createElement('li');
-                  el.innerText = data.Moods[i].keywords[j];
-                  moodList.appendChild(el);
-                }
-                for(let j in data.Moods[i].examples){
-                  let el = document.createElement('li');
-                  el.innerText = data.Moods[i].examples[j];
-                  examples.appendChild(el);
-                }
-                for(let j in data.Moods[i].body){
-                  let el = document.createElement('li');
-                  el.innerText = data.Moods[i].body[j];
-                  body.appendChild(el);
-                }
-                for(let j in data.Moods[i].mind){
-                  let el = document.createElement('li');
-                  el.innerText = data.Moods[i].mind[j];
-                  mind.appendChild(el);
-                }
-                for(let j in data.Moods[i].strategy){
-                  let el = document.createElement('li');
-                  el.innerText = data.Moods[i].strategy[j];
-                  strategy.appendChild(el);
-                }
+    document.getElementById("colorName").innerText = color;
+
+    for(let i in moodsData.Moods){ //each for loop within this one fills one of the lists
+        if(moodsData.Moods[i].color === color){
+            for(let j in moodsData.Moods[i].keywords){
+              let el = document.createElement('li');
+              el.innerText = moodsData.Moods[i].keywords[j];
+              moodList.appendChild(el);
+            }
+            for(let j in moodsData.Moods[i].examples){
+              let el = document.createElement('li');
+              el.innerText = moodsData.Moods[i].examples[j];
+              examples.appendChild(el);
+            }
+            for(let j in moodsData.Moods[i].body){
+              let el = document.createElement('li');
+              el.innerText = moodsData.Moods[i].body[j];
+              body.appendChild(el);
+            }
+            for(let j in moodsData.Moods[i].mind){
+              let el = document.createElement('li');
+              el.innerText = moodsData.Moods[i].mind[j];
+              mind.appendChild(el);
+            }
+            for(let j in moodsData.Moods[i].strategy){
+              let el = document.createElement('li');
+              el.innerText = moodsData.Moods[i].strategy[j];
+              strategy.appendChild(el);
             }
         }
-    });
+    }
 }
