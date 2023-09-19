34d437f904c9582ca26c8eb9e87034f1b25983a1
Io Bosse <io.bosse@protonmail.com>
2023-09-19 13:47:29 +0200

    lists are created dynamically, not finished

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ function showKeywords(){
 /*opens and closes pop up*/
 const modal = document.querySelector(".keywordPopup");
 const openModal = document.querySelectorAll(".colorbutton");
-const closeModal = document.querySelector(".close-button");
+
 
 for(i of openModal){
   i.addEventListener("click", () => {
@@ -38,34 +38,39 @@ for(i of openModal){
   });
 }
 
-closeModal.addEventListener("click", () => {
-  modal.close();
-});
-
-
-
 function display(color){ //is called using onclick for now, gets the button's ID as a parameter
 
-    document.getElementById("colorName").innerText = color;
+    let space = document.createElement('div');
 
     for(let i in moodsData.Moods){
         if(moodsData.Moods[i].color === color){
-            fillList(moodsData.Moods[i], "keywords");
-            fillList(moodsData.Moods[i], "examples");
-            fillList(moodsData.Moods[i], "body");
-            fillList(moodsData.Moods[i], "mind");
-            fillList(moodsData.Moods[i], "strategy");
+            space.append(fillList(moodsData.Moods[i], "keywords"));
+            //fillList(moodsData.Moods[i], "examples");
+            //fillList(moodsData.Moods[i], "body");
+            //fillList(moodsData.Moods[i], "mind");
+            //fillList(moodsData.Moods[i], "strategy");
         }
     }
+
+    popup.append(space);
+
+    const closeModal = document.querySelector(".close-button");
+    closeModal.addEventListener("click", () => {
+        modal.close();
+    });
 }
 
-function fillList(mood, list){ //fills the specified HTML list with correct content from the specified mood
 
-    document.getElementById(list).innerHTML = "";
-    
-    for(let i in mood[list]){
+function fillList(mood, listId){ //fills the specified HTML list with correct content from the specified mood
+
+    const popup = document.getElementById("popup");
+    let list = document.createElement('ul');
+
+    for(let i in mood[listId]){
         let el = document.createElement('li');
-        el.innerText = mood[list][i];
-        document.getElementById(list).appendChild(el);
+        el.innerText = mood[listId][i];
+        list.appendChild(el);
     }
+    
+    return list;
 }
