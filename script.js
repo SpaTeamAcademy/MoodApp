27ad38748c0cccf580234f4fd5f174c251ba24a2
Lea <stillerer@gmail.com>
2023-10-04 14:02:14 +0200

    moved field to right side and changed default color in inout

--- script.js
+++ script.js
@@ -216,9 +216,9 @@ function changeColor(id, input){
 //toggles visibility of color change option
 function showColors() {
   var x = document.getElementById("changeColor");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
+  if (x.style.display === "block") {
     x.style.display = "none";
+  } else {
+    x.style.display = "block";
   }
 }
